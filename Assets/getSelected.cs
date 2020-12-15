e6d1b2d261aeb5e4dc0d39341c5cc683daedb47e
johnPower <stkautoru@mail.ru>
2020-12-15 19:29:31 +0700

    init

---
+++ Assets/getSelected.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getSelected : MonoBehaviour
+{
+
+    //GameObject selected;
+    Text text;
+    public GameObject selected = null;
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                text.text = hit.transform.name;
+                selected = hit.collider.gameObject;
+                Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+            }
+        }
+    }
+}
